f3f217f0af7083ef3dfb9a21de14a09254ac99b9
Snail Mail <jonattham.felix@orbium.com.br>
2024-02-14 05:58:32 -0300

    feat: Modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,71 @@
-import { InputSearchNote } from "./components/input-search-note";
+import { ChangeEvent, useState } from "react";
 import { Line } from "./components/line";
 import { Logo } from "./components/logo";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
+
 export function App() {
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const noteOnStorage = localStorage.getItem("notes");
+    if (noteOnStorage) {
+      return JSON.parse(noteOnStorage);
+    }
+    return [];
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+    setSearch(query);
+  }
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+      : notes;
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <Logo />
 
-      <Logo/>
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight  outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
+        />
+      </form>
 
-      <InputSearchNote/>
-
-      <Line/>
+      <Line />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        <NewNoteCard/>
-        <NoteCard/>
-        <NoteCard/>
-
+        {filteredNotes.map((note) => {
+          return <NoteCard key={note.id} note={note} />;
+        })}
       </div>
-
     </div>
   );
 }
